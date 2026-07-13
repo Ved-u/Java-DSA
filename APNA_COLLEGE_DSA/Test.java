0cd7c47432fad33a093766cdbbe6c458570e6449
Ved-u <vedantutpat084@gmail.com>
2026-07-13 14:07:48 +0530

    Add variable assignments and output to Test.java

--- APNA_COLLEGE_DSA/Test.java
+++ APNA_COLLEGE_DSA/Test.java
@@ -1,5 +1,8 @@
 public class Test{
     public static void main(String args[]){
-        System.out.println("hello-world");
+        int a = 10;
+        long b = a;
+        int c = (int)b;
+        System.out.println("hello-world"+c);
     }
 }
