2100355633bdf5773355922452b7dd3e03758328
Ved-u <vedantutpat084@gmail.com>
2026-07-26 18:34:21 +0530

    streak

--- APNA_COLLEGE_DSA/Test.java
+++ APNA_COLLEGE_DSA/Test.java
@@ -1,8 +1,24 @@
 public class Test{
+    public static void binToDec(int bin) {
+        int dec = 0,pow = 0;
+        System.out.println("bin:"+bin);
+        while(bin > 0){
+            dec += bin%10*Math.pow(2,pow++);
+            bin/=10;
+            
+        }
+        System.out.println("dec:"+dec);
+    }
+    public static void dectoBin(int dec){
+        System.out.println("dec:"+dec);
+        int bin = 0;
+        while(dec>0){
+            bin = bin*10 + dec%2;
+            dec/=2;
+        }
+        System.out.println("bin:"+bin);
+    }
     public static void main(String args[]){
-        int a = 10;
-        long b = a;
-        int c = (int)b;
-        System.out.println("hello-world"+c);
+       dectoBin(7);
     }
 }
